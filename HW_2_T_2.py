0da974461fbb0b2d348ed763630697bdf12e7933
CEO82 <git-test@test.com>
2025-05-21 21:37:31 +0100

    4-th Commit HW_2_T_2, list creation made.

--- HW_2_T_2.py
+++ HW_2_T_2.py
@@ -4,3 +4,28 @@
 input().
 '''
 
+print(f'Program started \n')
+
+print(
+    f'You need to create the List of data, for add new data to the List press Y, to finish filling the List press N \n')
+
+user_list = list()
+
+while True:
+    yes_no_qest = input('Do you want enter new data to the List? Y or N: ')
+    if yes_no_qest.lower() != 'n':
+        user_list.append(input('Please enter any value: '))
+        print(user_list)
+        continue
+    else:
+        print(f'\nYour List is: {user_list}\n')
+        break
+
+list_lenth = len(user_list)
+
+if list_lenth % 2 != 0:
+    even_chk = 'non_even'
+
+counter = list_lenth
+
+while counter > 0:
