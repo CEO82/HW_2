145de6b7d54978d0ababecf76434c1d9b5632770
CEO82 <git-test@test.com>
2025-05-24 20:50:50 +0100

    10-th Commit HW_2_gpt_2, half done, need to solve age number problem in for in code

--- HW_2_gpt_2.py
+++ HW_2_gpt_2.py
@@ -5,5 +5,15 @@
 
 Укажи позиции (индексы), где они находятся.'''
 
-us_lst = [44, 11, 2, 4, 55, 49, 15]
+us_lst = [44, 11, 2, 4, 55, 49, 15, 88]
 
+max_d = 0
+min_d = 0
+
+for i in range(len(us_lst) - 1):
+    if us_lst[i] > us_lst[i +1] and us_lst[i] > max_d:
+        max_d = us_lst[i]
+    elif us_lst[i] < us_lst[i +1] and us_lst[i] < max_d:
+        min_d = us_lst[i]
+
+print(f'biggest number is {max_d} and smallest {min_d}')
