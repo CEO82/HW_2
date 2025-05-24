a721df87e7766e31d177a45c68ffba85a198ece4
CEO82 <git-test@test.com>
2025-05-24 21:48:12 +0100

    11-th Commit HW_2_gpt_2, correction 1

--- HW_2_gpt_2.py
+++ HW_2_gpt_2.py
@@ -19,8 +19,7 @@ else:
     min_ind = 0
 
 for i in range(len(us_lst) - 1):
-    value_i = us_lst[i]
-    value_i_plus = us_lst[i + 1]
+
     if us_lst[i] > us_lst[i + 1] and us_lst[i] > max_d:
         max_d = us_lst[i]
         max_ind = i
