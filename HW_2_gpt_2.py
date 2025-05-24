bc19e7e634f29f34358dbea1ee5a1a488792c730
CEO82 <git-test@test.com>
2025-05-24 21:58:16 +0100

    12-th Commit HW_2_gpt_2, correction 2, with hints from GPT

--- HW_2_gpt_2.py
+++ HW_2_gpt_2.py
@@ -5,34 +5,19 @@
 
 Укажи позиции (индексы), где они находятся.'''
 
-us_lst = [300, -6, 44, 11, 2, 4, 55, -1, 49, 15, 100, 88, -5, 200]
+us_lst = [-6, 44, 11, 2, 4, 55, -1, 49, 500, 15, 100, 88, -5, 200, 700]
 
-if us_lst[0] > us_lst[1]:
-    max_d = us_lst[0]
-    min_d = us_lst[1]
-    max_ind = 0
-    min_ind = 1
-else:
-    max_d = us_lst[1]
-    min_d = us_lst[0]
-    max_ind = 1
-    min_ind = 0
+max_d = us_lst[0]
+min_d = us_lst[0]
+max_ind = 0
+min_ind = 0
 
-for i in range(len(us_lst) - 1):
-
-    if us_lst[i] > us_lst[i + 1] and us_lst[i] > max_d:
+for i in range(len(us_lst)):
+    if us_lst[i] > max_d:
         max_d = us_lst[i]
         max_ind = i
-    elif us_lst[i] < us_lst[i + 1] and us_lst[i + 1] > max_d:
-        max_d = us_lst[i + 1]
-        max_ind = i + 1
-
-    if us_lst[i] < us_lst[i + 1] and us_lst[i] < min_d:
-        min_d = us_lst[i + 1]
+    if us_lst[i] < min_d:
+        min_d = us_lst[i]
         min_ind = i
-    elif us_lst[i] > us_lst[i + 1] and us_lst[i + 1] < min_d:
-        min_d = us_lst[i + 1]
-        min_ind = i + 1
-
 
 print(f'biggest number is: {max_d} with index: {max_ind}\nand smallest is: {min_d} with index: {min_ind}')
