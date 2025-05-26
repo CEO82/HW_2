1f4101938f7682111558b75ac9c311e7600d154a
CEO82 <git-test@test.com>
2025-05-26 21:23:47 +0100

    17-th Commit HW_2_gpt_4, solved by Enternet.

--- HW_2_GPT_4.py
+++ HW_2_GPT_4.py
@@ -8,4 +8,18 @@ input_list = [4, 5, 4, 6, 5, 7]
 # Ожидаемый результат: [4, 5, 6, 7]
 '''
 
+us_list = [1, 2, 3, 4, 3, 5, 6, 3, 7, 8, 9, 10, 7, 11]
+num_list = []
 
+unic_numb_list = list(set(us_list))
+
+print(us_list)
+print(f'{unic_numb_list} - The list made with set')
+
+for n in us_list:
+    if n in num_list:
+        continue
+    else:
+        num_list.append(n)
+
+print(f'{num_list} - The list made with cycle')
