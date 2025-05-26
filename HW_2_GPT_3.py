1ef55aad79b677d0eaac459179cb2656dffe0213
CEO82 <git-test@test.com>
2025-05-26 20:19:53 +0100

    16-th Commit HW_2_gpt_3, logic bug solved.

--- HW_2_GPT_3.py
+++ HW_2_GPT_3.py
@@ -9,17 +9,16 @@ my_list = [1, 2, 3, 2, 4, 2, 5]
 
 us_lst = [1, 3, 2, 5, 6, 7, 2, 9, 8]
 
-el_to_del = 2
+el_to_del = 10
 ind_cnt = 0
 a = 0
 
 if us_lst.count(el_to_del) < 1:
     a = 1
 
-for i in us_lst:
-    if i == el_to_del:
-        del us_lst[ind_cnt]
-    ind_cnt += 1 # need to fix logic
+while us_lst.count(el_to_del) > 0:
+    us_lst.pop(us_lst.index(el_to_del))
+
 
 if a == 1:
     print(f'The element {el_to_del} is not in the list {us_lst}')
