6167ac726c9f4a874138381841fe9f98107bdd8f
CEO82 <git-test@test.com>
2025-05-27 21:57:55 +0100

    20-th Commit HW_2_gpt_6.2, solved without set

--- HW_2_GPT_6.2.py
+++ HW_2_GPT_6.2.py
@@ -21,3 +21,38 @@ python
 
 Потом сделай с использованием set и сравни — какой вариант проще/понятнее.'''
 
+list1 = [1, 2, 3, 4, 5]
+list2 = [4, 5, 6, 7, 8, 8]
+prefinal_lst = []
+final_lst = []
+
+if len(list1) > len(list2):
+    main_lst = list2
+    second_lst = list1
+else:
+    main_lst = list1
+    second_lst = list2
+
+for i in main_lst:
+    if i in second_lst:
+        continue
+    else:
+        prefinal_lst.append(i)
+
+for i in second_lst:
+    if i in main_lst:
+        continue
+    else:
+        prefinal_lst.append(i)
+
+for i in prefinal_lst:
+    if prefinal_lst.count(i) > 1:
+        continue
+    else:
+        final_lst.append(i)
+
+print(f'List with unique numbers is - {final_lst}')
+
+
+
+
