a06ef1f2ff314c757a936713078aadfa9fdb05e4
CEO82 <git-test@test.com>
2025-05-27 20:57:28 +0100

    18-th Commit HW_2_gpt_5, solved by me

--- HW_2_GPT_5.py
+++ HW_2_GPT_5.py
@@ -1,5 +1,12 @@
 '''Задача 5: Переворот списка
 Пользователь вводит значения в список. Затем выведи этот список в обратном порядке (не используя .reverse() или [::-1]).'''
 
-my_list = [1, 2, 3, 4, 5, 6]
+my_list = [1, 2, 3, 4, 5, 6, 'r', -7.6]
+lst_lnh = len(my_list) #list length
+new_lst = []
 
+for v in my_list:
+    new_lst.append(my_list[lst_lnh - 1])
+    lst_lnh -= 1
+
+print(f'New list is: {new_lst}')
