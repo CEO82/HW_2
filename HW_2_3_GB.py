bb956ba73b9aa00659b91a429e4ab19ee300c5ac
CEO82 <git-test@test.com>
2025-05-28 20:39:34 +0100

    23-th Commit HW_2_3_GB.py, solved by me

--- HW_2_3_GB.py
+++ HW_2_3_GB.py
@@ -2,3 +2,20 @@
 относится месяц (зима, весна, лето, осень). Напишите решения через list и dict.
 '''
 
+while True:
+    try:
+        month_n = int(input("Please month number from 1 to 12: "))
+        if month_n < 1 or month_n > 12:
+            print("You are entered wrong month number")
+            continue
+        break
+    except ValueError:
+        print("You are entered wrong value, please try again.")
+
+month_lst = ['Jan', 'Feb', 'Mar', 'Apr', 'May', "Jun", 'Jul', "Aug", 'Sep', 'Oct', 'Nov', 'Dec']
+
+print(f'The number {month_n} month is - {month_lst[month_n - 1]}')
+
+month_dict = {1:'Jan', 2:'Feb', 3:'Mar', 4:'Apr', 5:'May', 6:'Jun', 7:'Jul', 8:'Aug', 9:'Sep', 10:'Oct', 11:'Nov', 12:'Dec'}
+
+print(f'\nThe number {month_n} month is - {month_dict.get(month_n)}')
