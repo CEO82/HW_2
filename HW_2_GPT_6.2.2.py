578b1c968a6ed5a8aa2dfc578019f32dd53a80b8
CEO82 <git-test@test.com>
2025-05-28 20:12:48 +0100

    22-th Commit HW_2_gpt_6.2.2, done

--- HW_2_GPT_6.2.2.py
+++ HW_2_GPT_6.2.2.py
@@ -20,8 +20,25 @@ python
 Постарайся сделать решение сначала без использования set, чтобы закрепить навыки.
 
 Потом сделай с использованием set и сравни — какой вариант проще/понятнее.'''
+from enum import unique
 
 list1 = [1, 2, 3, 4, 5]
 list2 = [4, 5, 6, 7, 8, 8]
 prefinal_lst = []
 final_lst = []
+
+unique = list((set(list1) ^ set(list2)))
+
+print(unique)
+
+unique_elements = []
+
+for num in list1:
+    if num not in list2 and num not in unique_elements:
+        unique_elements.append(num)
+
+for num in list2:
+    if num not in list1 and num not in unique_elements:
+        unique_elements.append(num)
+
+print(f'List with unique elements: {unique_elements}')
