8f91975b1c23684c20846beace2a273bc4ea12a6
CEO82 <git-test@test.com>
2025-06-10 21:49:16 +0100

    30-th Commit HW_2_6_GB.py, done

--- HW_2_6_GB.py
+++ HW_2_6_GB.py
@@ -19,3 +19,71 @@
 “количество”: [5, 2, 7],
 “ед”: [“шт.”]
 '''
+
+# tuple_lst = [(1, {})] # В списке должны быть кортежи а в кортежах порядковый номер и словарь с характеристиками товаров.
+
+tpl_cnt_nmb = 0  # Tuple counter
+tpls_lst = []  # List of Tuples
+
+while True:
+    tpl_cnt_nmb += 1
+    # it_name = {'Name' : input('Please enter item name: ')}
+    dct_fr_tpl = {'Name': input('Please enter item name: ')}  # #Dictionary for Tuple
+
+    while True:
+        try:
+            it_price = int(input("Please enter item price: "))
+            if it_price < 0:
+                print("You are entered negative price, please enter positive price: ")
+                continue
+            break
+        except ValueError:
+            print("You are entered wrong value, please try again.")
+    it_price = {'Price': it_price}
+    dct_fr_tpl.update(it_price)
+
+    while True:
+        try:
+            it_quantity = int(input("Please enter item it_quantity: "))
+            if it_quantity < 0:
+                print("You are entered negative it_quantity, please enter positive it_quantity: ")
+                continue
+            break
+        except ValueError:
+            print("You are entered wrong value, please try again.")
+
+    it_quantity = {'Quantity': it_quantity}
+    dct_fr_tpl.update(it_quantity)
+    it_units = {'Units': 'pcs'}
+    dct_fr_tpl.update(it_units)
+    basic_tuple = (tpl_cnt_nmb, dct_fr_tpl)
+
+    y_n_check = str(input(
+        'If you want to continue entering data please enter any value,\nif you want finish entering data please enter N or n : '))
+
+    tpls_lst.append(basic_tuple)
+
+    if y_n_check.lower() == 'n':
+        break
+
+
+analitic_dct = {}
+name_lst = []
+prc_lst = []
+qnt_lst = []
+unt_lst = []
+
+for i in tpls_lst:
+
+    i_dct = i[1]
+    name_lst.append(i_dct.get('Name'))
+    prc_lst.append(i_dct.get('Price'))
+    qnt_lst.append(i_dct.get('Quantity'))
+    unt_lst.append(i_dct.get('Units'))
+
+analitic_dct['Name'] = name_lst
+analitic_dct['Price'] = prc_lst
+analitic_dct['Quantity'] = qnt_lst
+analitic_dct['Units'] = unt_lst
+
+print(f'\nThe Analitic dictionary wil be:\n{analitic_dct}')
